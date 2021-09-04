daa0aa566c61ee5f8cbcb1bbd1e08dad9c507d49
isauljosue <56583387+isauljosue@users.noreply.github.com>
2021-09-04 14:00:48 -0400

    home path fix

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />  
+        <Route path="/" component={Home} />  
       </Switch>
       <Footer />
     </Router>
