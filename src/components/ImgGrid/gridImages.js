cf39d43d4b5e4e09000e06984b1663d9cc7bba6d
isauljosue <56583387+isauljosue@users.noreply.github.com>
2021-10-13 14:32:26 -0400

    relinked sponsor images

--- src/components/ImgGrid/gridImages.js
+++ src/components/ImgGrid/gridImages.js
@@ -1,10 +1,10 @@
 const gridImages = [
-    { id: 1, src: '/img/logo2.png', title: 'AC', description: 'logo' },
-    { id: 2, src: '/img/logo3.png', title: 'Vobi', description: 'logo' },
-    { id: 3, src: '/img/logo4.png', title: 'I.N. Network Security', description: 'logo' },
-    { id: 4, src: '/img/logo5.png', title: 'Opsu', description: 'logo' },
-    { id: 5, src: '/img/logo7.png', title: 'ʞeppa', description: 'logo' },
-    { id: 5, src: '/img/logo8.png', title: 'RGB', description: 'logo' },
+    { id: 1, src: '../../assets/logo2.png', title: 'AC', description: 'logo' },
+    { id: 2, src: '../../assets/logo3.png', title: 'Vobi', description: 'logo' },
+    { id: 3, src: '../../assets/logo4.png', title: 'I.N. Network Security', description: 'logo' },
+    { id: 4, src: '../../assets/logo5.png', title: 'Opsu', description: 'logo' },
+    { id: 5, src: '../../assets/logo7.png', title: 'ʞeppa', description: 'logo' },
+    { id: 5, src: '../../assets/logo8.png', title: 'RGB', description: 'logo' },
   ];
   
   export default gridImages;
